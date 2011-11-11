9473b95c10d137d8f2c1322fee26fcc77a93e241
sans-sense <apurba.n@imaginea.com>
2011-11-11 20:13:19 +0530

    removed dependence on temporary js variables

--- data/ff-integration.js
+++ data/ff-integration.js
@@ -1,15 +1,14 @@
 (function() {
-    var openFileWidget = document.getElementById('openFile');
-    var path;
-    openFileWidget.onclick = function(event) {
-        path = window.prompt("Enter js file name","/data/host/projects/personalProjects/fun/jseditor/scratch.js");
+    var openFileElement = document.getElementById('openFile');
+    openFileElement.onclick = function(event) {
+        var path = window.prompt("Enter js file name","/data/host/projects/personalProjects/fun/jseditor/scratch.js");
+        document.getElementById('ff-int-path').value = path;
         self.port.emit("openFile", path);
         return false;
     };
 
-    var saveFileWidget = document.getElementById('saveFile');
-    console.log(saveFileWidget);
-    saveFileWidget.onclick = function(event) {
+    var saveFileElement = document.getElementById('saveFile');
+    saveFileElement.onclick = function(event) {
         document.defaultView.postMessage("copyOld", "*");
         return false;
     }
@@ -20,13 +19,9 @@
         document.defaultView.postMessage("readNew", "*");
     });
 
-    self.port.on("readOld", function() {
-        self.port.emit("saveFile", {"path":path, "contents":document.getElementById('ff-int-field').value});
-    });
-
     document.defaultView.addEventListener("message", function(event) {
         if (event.data == "readOld") {
-            self.port.emit("saveFile", {"path":path, "contents":document.getElementById('ff-int-field').value});
+            self.port.emit("saveFile", {"path":document.getElementById('ff-int-path').value, "contents":document.getElementById('ff-int-field').value});
         }
     });
 }());
